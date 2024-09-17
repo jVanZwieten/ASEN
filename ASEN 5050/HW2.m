ced603a0c194b2f36927e6ffc2f01d0cc46ef641
John VanZwieten <john.vanzwieten@outlook.com>
2024-09-17 14:15:31 -0600

    complete 5050 HW2

--- ASEN 5050/HW2.m
+++ ASEN 5050/HW2.m
@@ -1,6 +1,6 @@
-clear
-
+format shortG
 % 1
+clear
 mu_mars = 4.305e4; % km^3/s^2
 r_marsEquator = 3397.2; % km
 R = [3.62067e3;
@@ -29,13 +29,67 @@ fprintf('%.6g', rad2deg(i))
 N = astroUtilities.LineOfNodesFromH(H)
 n = norm(N)
 
-Omega = astroUtilities.longitudeOfAscendingNodeFromN(N)
+Omega = astroUtilities.RAANFromN(N)
 fprintf('%.6g\n', rad2deg(Omega))
 
-omega = astroUtilities.argumentOfPeriapsisFromNE(N, E);
-omega = 2*pi - omega
+omega = -astroUtilities.argumentOfPeriapsisFromNE(N, E)
 fprintf('%.6g\n', rad2deg(omega))
 
-nu = astroUtilities.trueAnomalyFromRE(R, E);
-nu = 2*pi - nu
+nu = -astroUtilities.trueAnomalyFromRE(R, E)
 fprintf('%.6g\n', rad2deg(nu))
+
+theta = omega + nu
+fprintf('%.6g\n', rad2deg(theta))
+
+% b
+C = astroUtilities.DirectionCosineMatrix(Omega, omega, nu, i)
+Ct = C'
+Ct*C
+C*Ct
+
+R_radialFrame = Ct*R
+V_radialFrame = Ct*V
+
+phi = astroUtilities.flightPathAngleFromENu(e, nu)
+fprintf('%.6g\n', rad2deg(phi))
+
+V_radialFrame_0 = [v*sin(phi); v*cos(phi); 0]
+
+% c
+r_p = astroUtilities.periapsisFromAE(a, e)
+R_p = astroUtilities.PeriapsisFromRE(r_p, E)
+
+v_p = astroUtilities.velocityAtApsisFromHR(h, r_p)
+astroUtilities.specificMechanicalNRGFromrv(r_p, v_p, mu_mars)
+
+C_p = astroUtilities.DirectionCosineMatrix(Omega, omega, 0, i)
+
+V_p = C_p*[0; v_p; 0]
+cross(R_p, V_p)
+norm(cross(R_p, V_p))
+
+% % 2
+% clear
+% mu_moon = 4.9902799e3; % km^3/s^2
+% r_moonEquator = 1738; % km
+
+% Nhat = [0.6428; -0.766; 0];
+% Hhat = [-0.3237; -0.2717; 0.9063];
+% E = [0.0475; 0.3755; 0.1295];
+
+% % a
+% e = norm(E)
+% i = astroUtilities.inclinationFromHhat(Hhat)
+% fprintf('%.6g\n', rad2deg(i))
+% Omega = -astroUtilities.RAANFromNhat(Nhat)
+% fprintf('%.6g\n', rad2deg(Omega))
+% omega = astroUtilities.argumentOfPeriapsisFromNE(Nhat, E)
+% fprintf('%.6g\n', rad2deg(omega))
+
+% Ehat = E/e;
+% omega2 = acos(dot(Nhat, Ehat))
+
+% % b
+% r = 4070.6; % km
+
+% a = r*(1 + e*cos(-omega))/(1 - e^2)
--- ASEN 5050/astroUtilities.m
+++ ASEN 5050/astroUtilities.m
@@ -57,15 +57,23 @@ classdef astroUtilities
         end
 
         function i = inclinationFromH(H)
-            i = acos(H(3)/norm(H));
+            i = astroUtilities.inclinationFromHhat(H/norm(H));
+        end
+
+        function i = inclinationFromHhat(Hhat)
+            i = acos(Hhat(3));
         end
 
         function N = LineOfNodesFromH(H)
             N = [-H(2); H(1); 0];
         end
 
-        function Omega = longitudeOfAscendingNodeFromN(N)
-            Omega = acos(N(1)/norm(N));
+        function Omega = RAANFromN(N)
+            Omega = astroUtilities.RAANFromNhat(N/norm(N));
+        end
+
+        function Omega = RAANFromNhat(Nhat)
+            Omega = acos(Nhat(1));
         end
 
         function omega = argumentOfPeriapsisFromNE(N, E)
@@ -74,6 +82,30 @@ classdef astroUtilities
 
         function nu = trueAnomalyFromRE(R, E)
             nu = acos(dot(E, R)/(norm(E)*norm(R)));
+        end
+
+        function C = DirectionCosineMatrix(RAAN, AOP, trueAnomaly, inclination)
+            theta = AOP + trueAnomaly;
+            C = [cos(RAAN)*cos(theta) - sin(RAAN)*cos(inclination)*sin(theta), -cos(RAAN)*sin(theta) - sin(RAAN)*cos(inclination)*cos(theta), sin(RAAN)*sin(inclination);
+            sin(RAAN)*cos(theta) + cos(RAAN)*cos(inclination)*sin(theta), -sin(RAAN)*sin(theta) + cos(RAAN)*cos(inclination)*cos(theta), -cos(RAAN)*sin(inclination);
+            sin(inclination)*sin(theta), sin(inclination)*cos(theta), cos(inclination)];
+        end
+
+        function phi = flightPathAngleFromENu(e, nu)
+            phi = atan(e*sin(nu)/(1 + e*cos(nu)));
+        end
+
+        function R_p = RpFromAE(a, E)
+            r_p = astroUtilities.periapsisFromAE(a, norm(E));
+            R_p = PeriapsisFromRE(r_p, E);
+        end
+
+        function R_p = PeriapsisFromRE(r_p, E)
+            R_p = r_p*E/norm(E);
+        end
+
+        function v = velocityAtApsisFromHR(h, r)
+            v = h/r;
         end
     end
 end
