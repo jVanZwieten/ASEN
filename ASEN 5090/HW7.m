76aea0820b5b9377f45a2d1795ba6dbac358a8aa
John VanZwieten <john.vanzwieten@outlook.com>
2024-12-04 01:54:55 -0700

    5090 HW7 continued

--- ASEN 5090/HW7.m
+++ ASEN 5090/HW7.m
@@ -1,7 +1,7 @@
 close all; clear; clc; addpath("..")
 
 %% 2
-R_earth = CelestialParameters.radius_earth;
+R_earth = CelestialParameters.radius_earth/1000; % km
 mu_earth = CelestialParameters.gravityParameter_earth;
 
 alt_Cygnss = 520; % km
@@ -12,5 +12,9 @@ alt_Gps = 20200; % km
 r_Gps = R_earth + alt_Gps
 v_Gps = sqrt(mu_earth / r_Gps)
 
-v_GpsLos = v_Gps*r_cygnss/r_Gps
+v_GpsLos = v_Gps*r_Cygnss/r_Gps
 v_relMax = v_GpsLos + v_Cygnss
+
+c = Utilities.speedOfLight/1000; % km/s
+f_L1 = 1575.42e6; % Hz
+f_doppler = f_L1*v_relMax/c
